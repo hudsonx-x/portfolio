2a8152ecbfc643788e8932c31f8e1af4713cead5
hudsonx-x <sarahjhudson1304@gmail.com>
2024-08-25 17:48:31 +0100

    grabbed url from php superglobal

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
 <?php
+
+    // check if composer autoloader exists
+    // include it if it exists
+    if (file_exists('vendor/autoload.php')){
+        require_once('vendor/autoload.php');
+    }
+
+    $url = explode('?' , $_SERVER['REQUEST_URI'])[0];
+
+    var_dump($url);
+    
